8cacaf8cb2bddefb46f60f3993bee5c1205d7432
Zhiting Lin <zlin035@uottawa.ca>
2019-10-28 10:26:12 +0800

    update the new api address

--- src/models/bytom.js
+++ src/models/bytom.js
@@ -3,8 +3,8 @@ import Bytom from 'bytom-js-sdk'
 var networks = {
   testnetvapor: 'http://app.bycoin.io:3020/api/v1/vapor/',
   testnet: 'http://app.bycoin.io:3020/api/v1/btm/',
-  mainnet: 'https://api.bycoin.im:8000/api/v1/btm/',
-  mainnetvapor: 'https://api.bycoin.im:8000/api/v1/vapor/'
+  mainnet: 'https://bcapi.bystack.com/api/v1/btm/',
+  mainnetvapor: 'https://bcapi.bystack.com/api/v1/vapor/'
 }
 
 var bytom = new Bytom(networks, chrome.runtime.getURL('wasm/main.wasm'))
