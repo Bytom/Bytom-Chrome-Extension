b95df2d3b31ba2f7f0e4cd6c613f8cd257a7a77b
Zhiting Lin <zlin035@uottawa.ca>
2020-09-25 21:21:54 +0800

    fixed the signtransaction not reveal error

--- src/manifest.js
+++ src/manifest.js
@@ -2,7 +2,7 @@ module.exports = {
   manifest_version: 2,
   name: 'Byone',
   description: 'Bytom Wallet Chrome extension.',
-  version: '3.0.3',
+  version: '3.0.4',
   author: 'Bytom frontend, zhitinglin',
   web_accessible_resources: ['js/inject.js'],
   browser_action: {
